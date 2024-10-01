251d8db28a28eda4fcb733bc61c6c37dd5345045
Noahhhh <outlawsnoah@gmail.com>
2024-10-02 00:03:07 +0200

    Steam Workshop
    Steam Workshop

--- lua/autorun/cl_hud.lua
+++ lua/autorun/cl_hud.lua
@@ -1,55 +1,40 @@
---[[---------------------------------------------------------
-   Name: noah-hudfivem
-   Author : Noah
-   Desc: Hud FiveM Like
-   Version : 1.01
-   Last update : 22/09/2024
------------------------------------------------------------]]
-
 Noah_HUD = Noah_HUD or {}
 Noah_HUD.Config = Noah_HUD.Config or {}
-
 ----------------------- Configuration -----------------------
-
-local blacklistWeapons = {
-    ["weapon_physgun"] = true,
-    ["gmod_tool"] = true,
-    ["gmod_camera"] = true,
-}
-
-Noah_HUD.Config.Text = false  -- Affiche le texte
-Noah_HUD.MilimalisteMode = true -- Mode minimaliste
+Noah_HUD.Config.Text = true   -- Affiche le texte
+Noah_HUD.Config.MilimalisteMode = true -- Mode minimaliste (Affiche le job, le salaire et l'argent)
 
 ----------------------- Fin Configuration -----------------------
+
+RX = RX or function(x) return x / 1920 * ScrW() end
+RY = RY or function(y) return y / 1080 * ScrH() end
+
 surface.CreateFont("Noah_HUD:Font:1", {
-    font = "Arial",
-    size = 16,
-    weight = 500,
-    antialias = true
+    font = "Roboto Bold",
+    size = RY(20)
 })
 
 local MatHealth = Material("noah/hud/png/coeurs.png", "smooth")
 local MatArmor = Material("noah/hud/png/armor.png", "smooth")
 local MatMoney = Material("noah/hud/png/money.png", "smooth")
-local MatJob = Material("noah/hud/png/user_suit.png", "smooth")
-local MatSalary = Material("noah/hud/png/money_add.png", "smooth")
+local MatJob = Material("noah/hud/png/job.png", "smooth")
+local MatSalary = Material("noah/hud/png/salary.png", "smooth")
 local MatHunger = Material("noah/hud/png/hunger.png", "smooth")
 local MatGun = Material("noah/hud/png/gun.png", "smooth")
 
-hook.Add("HUDPaint", "Noah:HUD:AllGamemode", function()
+hook.Add("HUDPaint", "Noah:HUD:FiveM", function()
     local ply = LocalPlayer()
     local health = ply:Health()
     local armor = ply:Armor()
     local money = ply:getDarkRPVar("money")
+    local hunger = ply:getDarkRPVar("Energy")
+
     local job = ply:getDarkRPVar("job")
     local salary = ply:getDarkRPVar("salary")
-    local hunger = ply:getDarkRPVar("Energy")
 
     local health = math.Clamp(health, 0, 100)
     local armor = math.Clamp(armor, 0, 100)
-    local hunger = math.Clamp(hunger, 0, 100)
-
-    // Draw le fond
+    local hunger = math.ceil(ply:getDarkRPVar("Energy"))
 
     draw.RoundedBox(7, ScrW() - 210, 30, 200, 30, Color(44, 44, 44, 232))
     draw.RoundedBox(5, ScrW() - 210, 30, 35, 30, Color(44, 44, 44, 246))
@@ -60,7 +45,20 @@ hook.Add("HUDPaint", "Noah:HUD:AllGamemode", function()
     draw.RoundedBox(7, ScrW() - 210, 110, 200, 30, Color(44, 44, 44, 232))
     draw.RoundedBox(5, ScrW() - 210, 110, 35, 30, Color(44, 44, 44, 246))
 
+    if Noah_HUD.Config.MilimalisteMode then
+    draw.RoundedBox(7, ScrW() - 210, 150, 200, 30, Color(44, 44, 44, 232))
+    draw.RoundedBox(5, ScrW() - 210, 150, 35, 30, Color(44, 44, 44, 246))
+
+    draw.RoundedBox(7, ScrW() - 210, 190, 200, 30, Color(44, 44, 44, 232))
+    draw.RoundedBox(5, ScrW() - 210, 190, 35, 30, Color(44, 44, 44, 246))
+
+    draw.RoundedBox(7, ScrW() - 210, 230, 200, 30, Color(44, 44, 44, 232))
+    draw.RoundedBox(5, ScrW() - 210, 230, 35, 30, Color(44, 44, 44, 246))
+    end
+
     // Munition
+
+    if weaponsblackliste[LocalPlayer():GetActiveWeapon():GetClass()] then return end
 
     if !LocalPlayer():Alive() then return end
     if not IsValid(LocalPlayer():GetActiveWeapon()) then return end
@@ -71,21 +69,29 @@ hook.Add("HUDPaint", "Noah:HUD:AllGamemode", function()
 
     // Affiche les informations
 
-    draw.RoundedBox(9, ScrW() - 165, 40, health * 1, 9, Color(0, 138, 46))
+    draw.RoundedBox(9, ScrW() - 150, 40, 100, 9, Color(124, 124, 124))
+    draw.RoundedBox(9, ScrW() - 150, 40, health * 1, 9, Color(0, 138, 46))
     if Noah_HUD.Config.Text then
-    draw.DrawText("" .. health, "Noah_HUD:Font:1", ScrW() - 165, 30, Color(255, 255, 255), TEXT_ALIGN_LEFT)
+    draw.DrawText("" .. health, "Noah_HUD:Font:1", ScrW() - 110, 35, Color(255, 255, 255), TEXT_ALIGN_LEFT)
     end
 
-    draw.RoundedBox(9, ScrW() - 165, 80, armor * 1, 9, Color(0, 102, 255))
-
+    draw.RoundedBox(9, ScrW() - 150, 80, 100, 9, Color(124, 124, 124))
+    draw.RoundedBox(9, ScrW() - 150, 80, armor * 1, 9, Color(0, 102, 255))
     if Noah_HUD.Config.Text then
-    draw.DrawText("" .. armor, "Noah_HUD:Font:1", ScrW() - 165, 70, Color(255, 255, 255), TEXT_ALIGN_LEFT)
+    draw.DrawText("" .. armor, "Noah_HUD:Font:1", ScrW() - 110, 75, Color(255, 255, 255), TEXT_ALIGN_LEFT)
     end
 
-    draw.RoundedBox(9, ScrW() - 165, 120, health * 1, 9, Color(255, 102, 0))
-
+    draw.RoundedBox(9, ScrW() - 150, 120, 100, 9, Color(124, 124, 124))
+    draw.RoundedBox(9, ScrW() - 150, 120, hunger * 1, 9, Color(255, 102, 0))
     if Noah_HUD.Config.Text then
-        draw.DrawText("" .. hunger, "Noah_HUD:Font:1", ScrW() - 165, 110, Color(255, 255, 255), TEXT_ALIGN_LEFT)
+        draw.DrawText("" .. hunger, "Noah_HUD:Font:1", ScrW() - 110, 115, Color(255, 255, 255), TEXT_ALIGN_LEFT)
+    end
+
+
+    if Noah_HUD.Config.MilimalisteMode then
+    draw.SimpleText(job, "Noah_HUD:Font:1", ScrW() - 150, 163, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
+    draw.SimpleText(salary, "Noah_HUD:Font:1", ScrW() - 115, 203, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
+    draw.SimpleText(DarkRP.formatMoney(money), "Noah_HUD:Font:1", ScrW() - 165, 243, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
     end
 
     // Affiche les icones
@@ -102,12 +108,25 @@ hook.Add("HUDPaint", "Noah:HUD:AllGamemode", function()
     surface.SetDrawColor(255, 255, 255)
     surface.DrawTexturedRect(ScrW() - 200, 115, 16, 16)
 
-    // icones minition 
-
     surface.SetMaterial(MatGun)
     surface.SetDrawColor(255, 255, 255)
     surface.DrawTexturedRect(ScrW() - 209, 1035, 16, 16)
-   
+    
+    if Noah_HUD.Config.MilimalisteMode then
+
+    surface.SetMaterial(MatJob)
+    surface.SetDrawColor(255, 255, 255)
+    surface.DrawTexturedRect(ScrW() - 200, 155, 16, 16)
+
+
+    surface.SetMaterial(MatSalary)
+    surface.SetDrawColor(255, 255, 255)
+    surface.DrawTexturedRect(ScrW() - 200, 195, 16, 16)
+
+    surface.SetMaterial(MatMoney)
+    surface.SetDrawColor(255, 255, 255)
+    surface.DrawTexturedRect(ScrW() - 200, 235, 16, 16)
+    end
 end)
 
 local HideElement = {
@@ -127,4 +146,13 @@ local HideElement = {
 
 hook.Add("HUDShouldDraw", "Noah:RemoveHUD:Ndev", function(name)
     if HideElement[name] then return false end
-end)    
+end)
+
+--[[---------------------------------------------------------
+   Name: noah-hudfivem
+   Author : Noah
+   Desc: Hud FiveM
+   Version : 1.01
+   Last update : 22/09/2024
+   Discord : noah_ballas
+-----------------------------------------------------------]]
